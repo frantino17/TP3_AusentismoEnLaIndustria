cdedae7616adc89ecb3b259cdcde9e4802d0dbb4
Fran <frantino17@gmail.com>
2024-05-05 13:03:14 -0300

    segundo commit, se agrega el proyecto

---
+++ Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿
+namespace tp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtDiasSimulacion = new TextBox();
+            txtRemuneraciones = new TextBox();
+            txtCostosVariables = new TextBox();
+            txtValorVenta = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            btnSimulacion = new Button();
+            dgvSimulacion = new DataGridView();
+            txtDesdeDia = new TextBox();
+            txtCantidadDiasMostrar = new TextBox();
+            label6 = new Label();
+            label5 = new Label();
+            dgvProb = new DataGridView();
+            Obreros = new DataGridViewTextBoxColumn();
+            CantDias = new DataGridViewTextBoxColumn();
+            CantObreros = new Label();
+            txtCantidadObrerosNomina = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)dgvSimulacion).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dgvProb).BeginInit();
+            SuspendLayout();
+            // 
+            // txtDiasSimulacion
+            // 
+            txtDiasSimulacion.Location = new Point(232, 379);
+            txtDiasSimulacion.Margin = new Padding(3, 4, 3, 4);
+            txtDiasSimulacion.Name = "txtDiasSimulacion";
+            txtDiasSimulacion.Size = new Size(97, 27);
+            txtDiasSimulacion.TabIndex = 0;
+            // 
+            // txtRemuneraciones
+            // 
+            txtRemuneraciones.Location = new Point(232, 410);
+            txtRemuneraciones.Margin = new Padding(3, 4, 3, 4);
+            txtRemuneraciones.Name = "txtRemuneraciones";
+            txtRemuneraciones.Size = new Size(97, 27);
+            txtRemuneraciones.TabIndex = 1;
+            txtRemuneraciones.TextChanged += txtRemuneraciones_TextChanged;
+            // 
+            // txtCostosVariables
+            // 
+            txtCostosVariables.Location = new Point(232, 443);
+            txtCostosVariables.Margin = new Padding(3, 4, 3, 4);
+            txtCostosVariables.Name = "txtCostosVariables";
+            txtCostosVariables.Size = new Size(97, 27);
+            txtCostosVariables.TabIndex = 2;
+            // 
+            // txtValorVenta
+            // 
+            txtValorVenta.Location = new Point(232, 479);
+            txtValorVenta.Margin = new Padding(3, 4, 3, 4);
+            txtValorVenta.Name = "txtValorVenta";
+            txtValorVenta.Size = new Size(97, 27);
+            txtValorVenta.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(29, 382);
+            label1.Name = "label1";
+            label1.Size = new Size(164, 20);
+            label1.TabIndex = 4;
+            label1.Text = "Cantidad dias a simular";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(29, 482);
+            label2.Name = "label2";
+            label2.Size = new Size(104, 20);
+            label2.TabIndex = 5;
+            label2.Text = "Valor de venta";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(29, 446);
+            label3.Name = "label3";
+            label3.Size = new Size(116, 20);
+            label3.TabIndex = 6;
+            label3.Text = "Costos variables";
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(31, 413);
+            label4.Name = "label4";
+            label4.Size = new Size(118, 20);
+            label4.TabIndex = 7;
+            label4.Text = "Remuneraciones";
+            // 
+            // btnSimulacion
+            // 
+            btnSimulacion.Location = new Point(361, 33);
+            btnSimulacion.Margin = new Padding(3, 4, 3, 4);
+            btnSimulacion.Name = "btnSimulacion";
+            btnSimulacion.Size = new Size(164, 149);
+            btnSimulacion.TabIndex = 8;
+            btnSimulacion.Text = "Iniciar Simulacion";
+            btnSimulacion.UseVisualStyleBackColor = true;
+            btnSimulacion.Click += btnSimulacion_Click;
+            // 
+            // dgvSimulacion
+            // 
+            dgvSimulacion.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvSimulacion.Location = new Point(346, 209);
+            dgvSimulacion.Margin = new Padding(3, 4, 3, 4);
+            dgvSimulacion.Name = "dgvSimulacion";
+            dgvSimulacion.RowHeadersWidth = 51;
+            dgvSimulacion.Size = new Size(1294, 591);
+            dgvSimulacion.TabIndex = 9;
+            dgvSimulacion.CellContentClick += dgvSimulacion_CellContentClick;
+            // 
+            // txtDesdeDia
+            // 
+            txtDesdeDia.Location = new Point(232, 517);
+            txtDesdeDia.Margin = new Padding(3, 4, 3, 4);
+            txtDesdeDia.Name = "txtDesdeDia";
+            txtDesdeDia.Size = new Size(97, 27);
+            txtDesdeDia.TabIndex = 10;
+            txtDesdeDia.TextChanged += txtDesdeDia_TextChanged;
+            // 
+            // txtCantidadDiasMostrar
+            // 
+            txtCantidadDiasMostrar.Location = new Point(232, 556);
+            txtCantidadDiasMostrar.Margin = new Padding(3, 4, 3, 4);
+            txtCantidadDiasMostrar.Name = "txtCantidadDiasMostrar";
+            txtCantidadDiasMostrar.Size = new Size(97, 27);
+            txtCantidadDiasMostrar.TabIndex = 11;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(29, 559);
+            label6.Name = "label6";
+            label6.Size = new Size(166, 20);
+            label6.TabIndex = 13;
+            label6.Text = "Cantidad de iteraciones";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(29, 520);
+            label5.Name = "label5";
+            label5.Size = new Size(78, 20);
+            label5.TabIndex = 14;
+            label5.Text = "Dia Desde";
+            // 
+            // dgvProb
+            // 
+            dgvProb.AllowUserToAddRows = false;
+            dgvProb.AllowUserToDeleteRows = false;
+            dgvProb.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvProb.Columns.AddRange(new DataGridViewColumn[] { Obreros, CantDias });
+            dgvProb.Location = new Point(31, 33);
+            dgvProb.Name = "dgvProb";
+            dgvProb.RowHeadersWidth = 51;
+            dgvProb.Size = new Size(298, 256);
+            dgvProb.TabIndex = 15;
+            dgvProb.CellContentClick += dgvProb_CellContentClick;
+            // 
+            // Obreros
+            // 
+            Obreros.HeaderText = "Numero de obreros";
+            Obreros.MinimumWidth = 6;
+            Obreros.Name = "Obreros";
+            Obreros.ReadOnly = true;
+            Obreros.Width = 125;
+            // 
+            // CantDias
+            // 
+            CantDias.HeaderText = "Cantidad de dias";
+            CantDias.MinimumWidth = 6;
+            CantDias.Name = "CantDias";
+            CantDias.Width = 125;
+            // 
+            // CantObreros
+            // 
+            CantObreros.AutoSize = true;
+            CantObreros.Location = new Point(29, 343);
+            CantObreros.Name = "CantObreros";
+            CantObreros.Size = new Size(145, 20);
+            CantObreros.TabIndex = 17;
+            CantObreros.Text = "Cantidad de obreros";
+            // 
+            // txtCantidadObrerosNomina
+            // 
+            txtCantidadObrerosNomina.Location = new Point(232, 340);
+            txtCantidadObrerosNomina.Margin = new Padding(3, 4, 3, 4);
+            txtCantidadObrerosNomina.Name = "txtCantidadObrerosNomina";
+            txtCantidadObrerosNomina.Size = new Size(97, 27);
+            txtCantidadObrerosNomina.TabIndex = 16;
+            txtCantidadObrerosNomina.TextChanged += textBox1_TextChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1720, 869);
+            Controls.Add(CantObreros);
+            Controls.Add(txtCantidadObrerosNomina);
+            Controls.Add(dgvProb);
+            Controls.Add(label5);
+            Controls.Add(label6);
+            Controls.Add(txtCantidadDiasMostrar);
+            Controls.Add(txtDesdeDia);
+            Controls.Add(dgvSimulacion);
+            Controls.Add(btnSimulacion);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtValorVenta);
+            Controls.Add(txtCostosVariables);
+            Controls.Add(txtRemuneraciones);
+            Controls.Add(txtDiasSimulacion);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dgvSimulacion).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dgvProb).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private TextBox txtDiasSimulacion;
+        private TextBox txtRemuneraciones;
+        private TextBox txtCostosVariables;
+        private TextBox txtValorVenta;
+        private Label label1;
+        private Label label2; 
+        private Label label3; 
+        private Label label4;
+        private Button btnSimulacion;
+        private DataGridView dgvSimulacion;
+        private TextBox txtDesdeDia;
+        private TextBox txtCantidadDiasMostrar;
+        private Label label6;
+        private Label label5;
+        private DataGridView dgvProb;
+        private DataGridViewTextBoxColumn Obreros;
+        private DataGridViewTextBoxColumn CantDias;
+        private Label CantObreros;
+        private TextBox txtCantidadObrerosNomina;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,213 @@
+using System;
+using System.Windows.Forms;
+using System.Globalization;
+using System.Data;
+using System.Numerics;
+using System.Security.Cryptography;
+
+namespace tp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeSecondDataGridView();
+            // Configurar DataGridView
+            dgvSimulacion.ColumnCount = 10;
+            dgvSimulacion.Columns[0].Name = "Día";
+            dgvSimulacion.Columns[1].Name = "RND Ausentes";
+            dgvSimulacion.Columns[2].Name = "Cantidad ausentes";
+            dgvSimulacion.Columns[3].Name = "Cantidad obreros";
+            dgvSimulacion.Columns[4].Name = "Produccion";
+            dgvSimulacion.Columns[5].Name = "Ganancia Ventas";
+            dgvSimulacion.Columns[6].Name = "Materia prima";
+            dgvSimulacion.Columns[7].Name = "Mano de obra";
+            dgvSimulacion.Columns[8].Name = "Ganancia Diaria";
+            dgvSimulacion.Columns[9].Name = "Ganancia Acumulada";
+        }
+
+        private void Simular(double valorVenta, double costosVariables, double remuneraciones, int diasSimulacion, int desdeDia, int cantidadDiasMostrar, int cantidadObrerosNomina, List<int> valoresProb, int total)
+        {
+
+            // Limpiar DataGridView antes de agregar nuevos datos
+            dgvSimulacion.Rows.Clear();
+
+            double gananciaTotal = 0;
+            // Realizar la simulación para cada día
+            for (int dia = 1; dia <= diasSimulacion; dia++)
+            {
+                // Genera número aleatorio con 4 decimales
+                Random rand = new Random();
+                double numeroAleatorio = rand.NextDouble();
+                numeroAleatorio = Math.Round(numeroAleatorio, 4);
+
+                int totalObreros = cantidadObrerosNomina;
+
+                // Calcular el número de obreros ausentes
+                int obrerosAusentes = calcularObrerosAusentes(valoresProb, total, numeroAleatorio);
+
+
+                // Calcular el número de obreros presentes
+                int obrerosPresentes = totalObreros - obrerosAusentes;
+
+                // Determinar si la planta está en funcionamiento
+                bool plantaOperativa = true;
+                if (obrerosPresentes < 20)
+                {
+                    plantaOperativa = false;
+                }
+
+                // Calcular ingresos y costos
+                int ingresosDiarios = plantaOperativa ? (int)valorVenta : 0;
+                int materiaPrima = plantaOperativa ? (int)costosVariables : 0;
+                int costosManoObra = (int)(remuneraciones * obrerosPresentes);
+                double gananciaDiaria = ingresosDiarios - materiaPrima - costosManoObra;
+
+                // Actualizar la ganancia total
+                gananciaTotal += gananciaDiaria;
+
+                // Agregar fila a DataGridView solo si el día es mayor o igual a desdeDia
+                if (dia >= desdeDia && dgvSimulacion.Rows.Count <= cantidadDiasMostrar)
+                {
+                    dgvSimulacion.Rows.Add(dia, numeroAleatorio, obrerosAusentes, obrerosPresentes, plantaOperativa, ingresosDiarios, materiaPrima, costosManoObra, gananciaDiaria, gananciaTotal);
+                }
+
+
+            }
+
+            // Mostrar la ganancia total al final de la simulación
+            MessageBox.Show("La ganancia total durante " + diasSimulacion + " días es: $" + gananciaTotal);
+
+            // Limpiar los cuadros de texto
+            // LimpiarCampos();
+        }
+
+        private void LimpiarCampos()
+        {
+            // Limpiar los cuadros de texto
+            txtValorVenta.Clear();
+            txtCostosVariables.Clear();
+            txtRemuneraciones.Clear();
+            txtDiasSimulacion.Clear();
+            txtDesdeDia.Clear();
+            txtCantidadDiasMostrar.Clear();
+            txtCantidadObrerosNomina.Clear();
+        }
+
+        private bool CamposVacios()
+        {
+            // Verificar si alguno de los campos de texto está vacío
+            return string.IsNullOrWhiteSpace(txtValorVenta.Text) ||
+                   string.IsNullOrWhiteSpace(txtCostosVariables.Text) ||
+                   string.IsNullOrWhiteSpace(txtRemuneraciones.Text) ||
+                   string.IsNullOrWhiteSpace(txtDiasSimulacion.Text) ||
+                   string.IsNullOrWhiteSpace(txtDesdeDia.Text) ||
+                   string.IsNullOrWhiteSpace(txtCantidadDiasMostrar.Text) ||
+                   string.IsNullOrWhiteSpace(txtCantidadObrerosNomina.Text);
+
+        }
+        private int calcularObrerosAusentes(List<int> valoresProb, int total, double numeroAleatorio)
+        {
+
+            double probabilidadAcumulada = 0;
+            int obrerosAusentes = 0;
+            for (int i = 0; i < valoresProb.Count; i++)
+            {
+                double probabilidad = Math.Round((double)valoresProb[i] / total, 5);
+                probabilidadAcumulada += probabilidad;
+                if (numeroAleatorio < probabilidadAcumulada)
+                {
+                    obrerosAusentes = i;
+
+                    return i;
+                }
+            }
+            return 0;
+        }
+        private void btnSimulacion_Click(object sender, EventArgs e)
+        {
+            // Verificar si algún campo está vacío
+            if (CamposVacios())
+            {
+                MessageBox.Show("Por favor, complete todos los campos antes de continuar.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return; // Salir del método sin continuar la simulación
+            }
+
+            // Convertir los valores de los campos de texto a tipos numéricos
+            double valorVenta = double.Parse(txtValorVenta.Text);
+            double costosVariables = double.Parse(txtCostosVariables.Text);
+            double remuneraciones = double.Parse(txtRemuneraciones.Text);
+            int diasSimulacion = int.Parse(txtDiasSimulacion.Text);
+            int desdeDia = int.Parse(txtDesdeDia.Text);
+            int cantidadDiasMostrar = int.Parse(txtCantidadDiasMostrar.Text);
+            int cantidadObrerosNomina = int.Parse(txtCantidadObrerosNomina.Text);
+
+            List<int> valoresProb = new List<int>();
+            int total = 0;
+            foreach (DataGridViewRow row in dgvProb.Rows)
+            {
+                if (row.Cells["CantDias"].Value != null && int.TryParse(row.Cells["CantDias"].Value.ToString(), out int valor))
+                {
+                    valoresProb.Add(valor);
+                    total += valor;
+                }
+                else
+                {
+                    valoresProb.Add(0);
+                }
+            }
+
+            // Llamar a la función de simulación con los parámetros ingresados
+            Simular(valorVenta, costosVariables, remuneraciones, diasSimulacion, desdeDia, cantidadDiasMostrar, cantidadObrerosNomina, valoresProb, total);
+        }
+
+
+        private void InitializeSecondDataGridView()
+        {
+            // Agrega cinco filas iniciales
+            for (int i = 0; i < 6; i++)
+            {
+                dgvProb.Rows.Add(i.ToString());
+            }
+
+            // Impide que los usuarios agreguen filas
+            dgvProb.AllowUserToAddRows = false;
+        }
+
+        private void txtRemuneraciones_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtDesdeDia_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgvSimulacion_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgvProb_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
